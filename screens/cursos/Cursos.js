6015fd331d6937cd4ac61dc541f1100493152665
Helder2609 <helder.muniz@iesb.edu.br>
2023-10-16 21:45:25 -0300

    aula1

--- screens/cursos/Cursos.js
+++ screens/cursos/Cursos.js
@@ -1,37 +1,53 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import React, { useEffect, useState } from 'react'
-import { Button, Text } from 'react-native-paper'
+import { Button, Card, Divider, FAB, IconButton, Text } from 'react-native-paper'
+import { useFocusEffect } from '@react-navigation/native';
+import { ScrollView } from 'react-native';
 
 const Cursos = ({ navigation }) => {
 
   const [cursos, setCursos] = useState([])
 
-  useEffect(() => {
-    AsyncStorage.getItem('cursos').then(resultado => {
-      
-      resultado = JSON.parse(resultado) || []
+  useFocusEffect(
+    React.useCallback((a) => {
+      AsyncStorage.getItem('cursos').then(resultado => {
 
-      console.log(resultado)
-      setCursos(resultado)
-    })
-  }, [])
+        resultado = JSON.parse(resultado) || []
+
+        console.log(resultado)
+        setCursos(resultado)
+      })
+    }, [])
+  );
 
   return (
     <>
-      <Text>Cursos</Text>
+      <ScrollView style={{ padding: 15 }}>
+        
 
-      {cursos.map(item=>(
-        <Text>{item.nome}</Text>
-      ))}
-      
-      <Button
-        icon='plus'
-        mode='contained'
-        onPress={() => navigation.push('cursos-form')}
-      >
-        Novo
-      </Button>
+        {cursos.map((item, i) => (
+          <Card key={i} mode='outlined' style={{ marginBottom: 5 }}>
+            <Card.Content margintop={10}>
+              <Text variant="titleLarge">{item.nome}</Text>
+              <Text variant="bodyMedium">Duração: {item.duracao + ' Semestres'}</Text>
+              <Text varianqt="bodyMedium">Modalidade: {item.modalidade}</Text>
+            </Card.Content>
+            <Card.Actions>
+              <IconButton buttonColor='#9370DB' icon='pencil-outline' />
+              <IconButton icon='trash-can-outline' onClick={() => this.deletarItem(i)} />
+            </Card.Actions>
+          </Card>
+        ))}
 
+
+      </ScrollView>
+        <FAB
+          icon="plus"
+          style={{
+            position: 'absolute', right: 5, bottom: 2,
+          }}
+          onPress={() => navigation.push('cursos-form')}
+        />
     </>
   )
 }
